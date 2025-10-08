fd78e7714236f985ec4ff369a151fb60cee22a54
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-10-08 08:39:07 -0300

    new string exercise

---
+++ src/main/java/org/desviante/string/MoveTheFirstNCharacters/Main.java
@@ -0,0 +1,19 @@
+package org.desviante.string.MoveTheFirstNCharacters;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        int n = scanner.nextInt();
+        if (n > 0 && n <= word.length()) {
+            String substring = word.substring(n) + word.substring(0, n);
+            System.out.println(substring);
+        } else {
+            System.out.println(word);
+        }
+        scanner.close();
+    }
+}
