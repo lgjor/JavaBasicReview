df673a41a90137c3f597d6427fb389da769aea03
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-10-11 08:23:14 -0300

    Return exercise

---
+++ src/main/java/org/desviante/method/InABox/Box.java
@@ -0,0 +1,13 @@
+package org.desviante.method.InABox;
+
+public class Box {
+
+    double height;
+    double width;
+    double length;
+
+    // write a method here
+    public double getVolume(){
+        return height * width * length;
+    };
+}
